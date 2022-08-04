c7ee44bbc2adbb8c932786c753cc419e27138437
calisthenicsGuy <radevradoslav12@gmail.com>
2022-08-04 15:02:01 +0300

    migrations added

--- Car Renting System/CarRentingSystem/CarRentingSystem/Data/CarRentingDbContext.cs
+++ Car Renting System/CarRentingSystem/CarRentingSystem/Data/CarRentingDbContext.cs
@@ -7,33 +7,21 @@
 
     public class CarRentingDbContext : IdentityDbContext
     {
-        private readonly IConfiguration configuration;
-
-        public CarRentingDbContext(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
-
         public CarRentingDbContext(DbContextOptions<CarRentingDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Car> Cars { get; init; }
-        public DbSet<Category> Categories { get; init; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-
-            }
-
-            base.OnConfiguring(optionsBuilder);
-        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder
+                .Entity<Car>
+                .HasOne(c => c.Category)
+                .WithMany(c => c.Cars).HasForeignKey(c => c.CategoryId);
+
             base.OnModelCreating(builder);
         }
     }
---
+++ Car Renting System/CarRentingSystem/CarRentingSystem/Data/Migrations/20220804115105_CarsAndCategoriesTablesAdded.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentingSystem.Data.Migrations
+{
+    public partial class CarsAndCategoriesTablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", maxLength: 10000, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cars_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_CategoryId",
+                table: "Cars",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
